f488d42ceb1c40cbd6c998ee80b7ea4564b20781
олег <gorodeckiy88@gmail.com>
2021-12-01 23:02:03 +0200

    first commit

---
+++ New-Project/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('.menu-open').click(function () {
+    $('.menu__list').toggleClass('show');
+  });
+});
+$(function () {
+  $('.menu-phone').click(function () {
+    $('.header__phone').toggleClass('call');
+  });
+});
